2b1eaf35aa7e606a2ca5bd0599809d633248409e
destrike00 <k.aron147@gmail.com>
2023-05-28 11:21:34 +0200

    runnable window with looped music

---
+++ main.py
@@ -0,0 +1,39 @@
+from settings import *
+from machine import Machine
+import pygame,sys
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen=pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption('The Casino')
+        self.clock=pygame.time.Clock()
+        self.bg_image=pygame.image.load(BG_IMAGE_PATH)
+
+        #create slotmachine class
+        self.machine=Machine()
+        self.delta_time=0
+
+        main_sound=pygame.mixer.Sound("audio/track.mp3")
+        main_sound.play(loops=-1)
+
+    def run(self):
+
+        while True:
+            self.start_time = pygame.time.get_ticks()
+            for event in pygame.event.get():
+                if event.type==pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.delta_time=(pygame.time.get_ticks()-self.start_time)/1000
+            self.start_time=pygame.time.get_ticks()
+
+            pygame.display.update()
+            self.screen.blit(self.bg_image,(0,0))
+            self.machine.update(self.delta_time)
+            self.clock.tick(FPS)
+
+
+if __name__=='__main__':
+    game=Game()
+    game.run()
+
---
+++ settings.py
@@ -0,0 +1,8 @@
+#Display settings
+
+DEFAULT_IMAGE_SIZE=(300,300)
+FPS=144
+HEIGHT=900
+WIDTH=1280
+
+BG_IMAGE_PATH='graphics/0/bg.png'
